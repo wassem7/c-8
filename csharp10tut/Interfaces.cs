e4790683e467c17a85d97444217a72445ebebb1a
wassem7 <waseemdarkwa@gmail.com>
2023-11-15 09:37:40 +0000

    interfaces

---
+++ csharp10tut/Interfaces.cs
@@ -0,0 +1,37 @@
+interface ICustomer
+{
+    void Print();
+    double Balance(double balance);
+}
+
+interface ICustomer1
+{
+    void Print1();
+}
+
+class Customer1 : ICustomer, ICustomer1
+{
+    public void Print()
+    {
+        Console.WriteLine("Void Print Method");
+    }
+
+    public void Print1()
+    {
+        System.Console.WriteLine("Interface I2 Method");
+    }
+
+    public double Balance(double balance)
+    {
+        return balance;
+    }
+}
+
+class Interfaces
+{
+    static void Main()
+    {
+        Customer1 C1 = new Customer1();
+        ICustomer C2 = new Customer1();
+    }
+}
