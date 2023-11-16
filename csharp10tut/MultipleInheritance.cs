ba05f72e70610dad25ba5198c5cd1dfa72ccd4e5
wassem7 <waseemdarkwa@gmail.com>
2023-11-16 12:02:25 +0000

    Multiple inheritance with interfaces

---
+++ csharp10tut/MultipleInheritance.cs
@@ -0,0 +1,48 @@
+interface IA
+{
+    void PrintA();
+}
+
+interface IB
+{
+    void PrintB();
+}
+
+class A : IA
+{
+    public void PrintA()
+    {
+        System.Console.WriteLine("Interface A Method");
+    }
+}
+
+class B : IB
+{
+    public void PrintB()
+    {
+        System.Console.WriteLine("Interface B Method");
+    }
+}
+
+class AB : IA, IB
+{
+    A a = new A();
+    B b = new B();
+
+    public void PrintA()
+    {
+        a.PrintA();
+    }
+
+    public void PrintB()
+    {
+        b.PrintB();
+    }
+
+    static void Main()
+    {
+        var a = new AB();
+        a.PrintA();
+        a.PrintB();
+    }
+}
