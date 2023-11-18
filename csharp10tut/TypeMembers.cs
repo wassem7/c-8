55a7cb798fc7bcf1cc9376aab9a396276819c725
wassem7 <waseemdarkwa@gmail.com>
2023-11-18 04:08:34 +0000

    Type members

---
+++ csharp10tut/TypeMembers.cs
@@ -0,0 +1,39 @@
+class VipCustomer
+{
+    #region Fields
+    private int _id;
+    private string _firstname;
+
+    private string _lastname;
+
+    #endregion
+
+
+    #region Public Properties
+    public int ID
+    {
+        get { return this._id; }
+        set { this._id = value; }
+    }
+
+    public string FirstName
+    {
+        get { return this._firstname; }
+        set { this._firstname = value; }
+    }
+
+    public string LastName
+    {
+        get { return this._lastname; }
+        set { this._lastname = value; }
+    }
+    #endregion
+
+    #region Methods
+    public void GetFullName()
+    {
+        System.Console.WriteLine($"FullName : {this._firstname} {this._lastname}");
+    }
+
+    #endregion
+}
